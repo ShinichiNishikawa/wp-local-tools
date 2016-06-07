7c52fcde6ebb7f656282a9cbe1d9fd7a0895111c
Shinichi Nishikawa <shinichi.nishikawa@gmail.com>
2016-06-07 17:24:24 +0900

    fix wrong comment indicator

--- wp-init.sh
+++ wp-init.sh
@@ -4,24 +4,24 @@
 DIRNAME=${PWD##*/}
 HTTP_HOST='$_SERVER["HTTP_HOST"]'
 
-// Download WordPress
+# Download WordPress
 wp core download
 
-// Create wp-config.php, adding consonants.
+# Create wp-config.php, adding consonants.
 wp core config --dbname=${DIRNAME} --dbuser=root --dbpass=root --extra-php <<PHP
 define( 'WP_DEBUG', true );
 define( 'WP_SITEURL', 'http://'.${HTTP_HOST} );
 define( 'WP_HOME',    'http://'.${HTTP_HOST} );
 PHP
 
-// create a database
+# create a database
 wp db create
 
-// Retrieve Movefile template for WordMove
+# Retrieve Movefile template for WordMove
 curl https://raw.githubusercontent.com/ShinichiNishikawa/wp-local-tools/master/resources/Movefile > Movefile
 
-// Retrieve .gitignore template
+# Retrieve .gitignore template
 curl https://raw.githubusercontent.com/ShinichiNishikawa/wp-local-tools/master/resources/WordPress.gitignore > .gitignore
 
-// Retrieve wp-cli.local.yml template for WP-CLI
+# Retrieve wp-cli.local.yml template for WP-CLI
 curl https://raw.githubusercontent.com/ShinichiNishikawa/wp-local-tools/master/resources/wp-cli.local.yml > wp-cli.local.yml
